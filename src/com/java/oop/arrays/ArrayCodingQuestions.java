c1097973b3acaeeaac65fe4fe1829024ec1743c0
madaram-akhil-dev <akhilmadaram@gmail.com>
2026-04-23 00:47:07 +0530

    feat: added array-based method to find highest product price

---
+++ src/com/java/oop/arrays/ArrayCodingQuestions.java
@@ -0,0 +1,22 @@
+package com.java.oop.arrays;
+
+public class ArrayCodingQuestions {
+
+    public static int findHighestPrice(int[] productPrices) {
+        int max = productPrices[0];
+
+        for (int i = 1; i < productPrices.length; i++) {
+            if (productPrices[i] > max) {
+                max = productPrices[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] productPrices = {10, 20, 30, 40, 50};
+
+        System.out.println(findHighestPrice(productPrices));
+    }
+}
