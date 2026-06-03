a1e7e21d6576772eb4b9b84f2d90ccdf5176c4c6
madaram-akhil-dev <akhilmadaram@gmail.com>
2026-06-03 15:52:40 +0530

    feat: demonstrate custom exceptions, throw/throws, and multi-catch handling

---
+++ src/com/java/oop/exceptions/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package com.java.oop.exceptions;
+
+public class InsufficientFundsException extends Exception {
+
+    public InsufficientFundsException(String message){
+        super(message);
+    }
+}
